47266477fd15a1fec935b136690767b6e2a8891f
Adrien Burgess <a.burke.burgess@gmail.com>
2025-05-06 19:23:59 -0300

    git conflict keys

--- lua/plugins/gitconflict.lua
+++ lua/plugins/gitconflict.lua
@@ -3,5 +3,51 @@ return {
     "akinsho/git-conflict.nvim",
     version = "*",
     config = true,
+    keys = {
+      {
+        "<leader>gm",
+        desc = "Git Conflict",
+      },
+      {
+        "<leader>gmc",
+        "<cmd>GitConflictChooseOurs<cr>",
+        desc = "Choose current",
+      },
+      {
+        "<leader>gmc",
+        "<cmd>GitConflictChooseOurs<cr>",
+        desc = "Choose current",
+      },
+      {
+        "<leader>gmt",
+        "<cmd>GitConflictChooseTheirs<cr>",
+        desc = "Choose incoming",
+      },
+      {
+        "<leader>gmb",
+        "<cmd>GitConflictChooseBoth<cr>",
+        desc = "Choose both",
+      },
+      {
+        "<leader>gmn",
+        "<cmd>GitConflictChooseNone<cr>",
+        desc = "Choose none",
+      },
+      {
+        "<leader>gm]",
+        "<cmd>GitConflictNextConflict<cr>",
+        desc = "Move to next conflict",
+      },
+      {
+        "<leader>gm[",
+        "<cmd>GitConflictPrevConflict<cr>",
+        desc = "Move to next conflict",
+      },
+      {
+        "<leader>gmq",
+        "<cmd>GitConflictListQf<cr>",
+        desc = "Quick fix",
+      },
+    },
   },
 }
